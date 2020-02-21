83d1edb85281e6959b4e16486627a3eb20ef6436
NoSkillGirl <singhpooja240393@gmail.com>
2020-02-21 18:14:32 +0530

    Golang certificate added

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@
  zip -r ~/workspace/pooja/my-portfolio.zip ~/workspace/pooja/my-portfolio -x '*.git*'
  echo "---------------------------"
  echo "Coping local zip to server"
- scp -i ~/.ssh/my_portfolio ~/workspace/pooja/my-portfolio.zip ajoop1524@35.200.153.183:
- ssh -i ~/.ssh/my_portfolio ajoop1524@35.200.153.183 "rm -rf my-portfolio && rm -rf __MACOSX//"
+ scp -i ~/.ssh/id_rsa ~/workspace/pooja/my-portfolio.zip singhpooja240393@34.82.103.62:
+ ssh -i ~/.ssh/id_rsa singhpooja240393@34.82.103.62 "rm -rf my-portfolio && rm -rf __MACOSX//"
  echo "---------------------------"
  echo "Unzipping in server"
- ssh -i ~/.ssh/my_portfolio ajoop1524@35.200.153.183 "unzip -o my-portfolio.zip"
- ssh -i ~/.ssh/my_portfolio ajoop1524@35.200.153.183 "rm my-portfolio.zip"
+ ssh -i ~/.ssh/id_rsa singhpooja240393@34.82.103.62 "unzip -o my-portfolio.zip"
+ ssh -i ~/.ssh/id_rsa singhpooja240393@34.82.103.62 "rm my-portfolio.zip"
  echo "---------------------------"
  echo "Successfully Deployed"
